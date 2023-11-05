1ee7bf310f9553dd6429ca713f31533334c973c0
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-11-05 12:21:00 +0200

    phonebook works

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  // <>
+  //   <App />
+  //   <ToastContainer autoClose={1500} />
+  // </>
+  <React.StrictMode basename="/vugar-gasimov">
     <App />
     <ToastContainer autoClose={1500} />
-  </>
+  </React.StrictMode>
 );
-
-/* <React.StrictMode basename="/vugar-gasimov">
-  <App />
-  <ToastContainer autoClose={1500} />
-</React.StrictMode>; */
